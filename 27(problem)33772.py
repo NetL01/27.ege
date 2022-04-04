869ad6f932fc22e555e387793718ff6cf74b2ec5
Nikita Razuvaev <48290510+NetL01@users.noreply.github.com>
2022-04-04 23:02:58 +0300

    Add files via upload

--- 27(problem)33772.py
+++ 27(problem)33772.py
@@ -1,18 +1,29 @@
 # четный min_diff
-min_diff_even = -1
+min_diff_even = []
 
 # нечётный min_diff
-min_diff_odd = -1
+min_diff_odd = []
 count = 0
+count_even = 0
+count_odd = 0
 with open('27_a.txt', 'r') as f:
     rounds = int(f.readline())
     for i in range(rounds):
         a, b = list(map(int, f.readline()))
-        count += min(a, b)
+        
         if min(a, b) % 2 == 0:
-            min_diff_even = min(min_diff_even, min(a, b))
+            min_diff_even.append(min(a, b))
+            count += min(a, b)
+            count_even += 1
         if min(a, b) % 2 != 0:
-            min_diff_odd = min(min_diff_odd, min(a, b))
+            min_diff_odd.append(min(a, b))
+            count += min(a, b)
+            count_odd += 1
+    if count_even > count_odd and count % 2 == 0:
+        print(count)
+    if count_even > count_odd and count % 2 != 0:
+        print(count)
+        
                             
         
         
